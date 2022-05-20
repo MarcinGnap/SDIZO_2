25416869a88de4a6a3189879efad084e9471a38d
Marcin Gnap <258953@student.pwr.edu.pl>
2022-05-20 21:29:16 +0200

    1.4 - Less than basic array commit.

--- pt_p_13_15-k02_10j-258953_lista_2/array.cpp
+++ pt_p_13_15-k02_10j-258953_lista_2/array.cpp
@@ -1,1 +1,31 @@
 #include "array.h"
+
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+array::array()
+{
+
+}
+
+array::~array()
+{
+
+}
+
+void array::push()
+{
+
+}
+
+void array::pop()
+{
+
+}
+
+void array::clearAllArray()
+{
+
+}
--- pt_p_13_15-k02_10j-258953_lista_2/array.h
+++ pt_p_13_15-k02_10j-258953_lista_2/array.h
@@ -1,5 +1,15 @@
 #pragma once
 class array
 {
-};
+public:
+	int iTSize;
+	int *newArray = nullptr;
+	
+	array();
+	~array();
 
+	void push();
+	void pop();
+	void clearAllArray();
+
+};
